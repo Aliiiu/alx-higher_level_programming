443da9a2358ee81195bfce3c1614aa986896cd1b
aliiiu <ayokunumi99@gmail.com>
2022-08-09 17:50:30 -0700

    UPDATE: done with compulsory

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 -- script that creates a table called first_table in the current database in my MySQL server.
-CREATE DATABASE IF NOT EXISTS `first_table` {`id` INT, `name` VARCHAR(256)};
+CREATE DATABASE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
